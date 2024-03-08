08c9a82f1b7423c917be53c999eade1609e3a0db
n521459 <adrian.casasd@gruposantander.com>
2024-03-08 14:38:01 +0100

    update

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ fastify.get('/', async function handler (request, reply) {
 const start = async () => {
   // Run the server!
   try {
-    await fastify.listen({ port: 80 })
+    await fastify.listen({ port: 3000 })
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
