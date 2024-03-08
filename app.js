15e8330a1e2137ee4a977cb959a03f706257df70
n521459 <adrian.casasd@gruposantander.com>
2024-03-08 21:13:45 +0100

    hello world

--- app.js
+++ app.js
@@ -29,20 +29,15 @@ io.use((socket, next) => {
 
 io.on('connection', (socket) => {
   console.log("user connected!");
-  /*
-  socket.on('room', room => {
-    console.log("new channel created: " +  room);
-    socket.join(room);
-  });*/
-  /*
-  socket.on('message', data => {
-    console.log("Message from client: " + data);
-    io.emit('message',data);
-  });*/
+  
 });
 
 server.listen(3000, () => {
     console.log('listening on *:3000');
+});
+
+app.get('/', async (req, res) => {
+  res.json({ hello: 'world' })
 });
 
 
@@ -52,36 +47,3 @@ app.post('/api/teams', async (req, res) => {
   io.emit('teams_message',req.body);
   res.json({ result: 'ok' })
 });
-
-
-
-/*
-socket.emit('message', "this is a test"); //sending to sender-client only
-
-socket.broadcast.emit('message', "this is a test"); //sending to all clients except sender
-
-socket.broadcast.to('game').emit('message', 'nice game'); //sending to all clients in 'game' room(channel) except sender
-
-socket.to('game').emit('message', 'enjoy the game'); //sending to sender client, only if they are in 'game' room(channel)
-
-socket.broadcast.to(socketid).emit('message', 'for your eyes only'); //sending to individual socketid
-
-io.emit('message', "this is a test"); //sending to all clients, include sender
-
-io.in('game').emit('message', 'cool game'); //sending to all clients in 'game' room(channel), include sender
-
-io.of('myNamespace').emit('message', 'gg'); //sending to all clients in namespace 'myNamespace', include sender
-
-socket.emit(); //send to all connected clients
-
-socket.broadcast.emit(); //send to all connected clients except the one that sent the message
-
-socket.on(); //event listener, can be called on client to execute on server
-
-io.sockets.socket(); //for emiting to specific clients
-
-io.sockets.emit(); //send to all connected clients (same as socket.emit)
-
-io.sockets.on() ; //initial connection from a client.
-
-*/
