fc8bc37145d6cd8ada18b21540038ed46f8ed304
n521459 <adrian.casasd@gruposantander.com>
2024-03-08 14:10:53 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const fastify = require('fastify')({
+  logger: true
+});
+
+// Declare a route
+fastify.get('/', async function handler (request, reply) {
+  return { hello: 'world' }
+})
+
+
+
+
+
+const start = async () => {
+  // Run the server!
+  try {
+    await fastify.listen({ port: 4001 })
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+
+start();
+
